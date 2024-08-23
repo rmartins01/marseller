a6fd82e3d57963f4b74bb7746e3506fa84fd45b9
Robson Martins <robson.java@gmail.com>
2024-08-23 18:17:36 -0300

    first commit

---
+++ src/main/java/br/com/marseller/MarsellerApplication.java
@@ -0,0 +1,13 @@
+package br.com.marseller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsellerApplication.class, args);
+	}
+
+}
